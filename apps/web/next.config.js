15d13a974141d97862141bd59cc650c6f016698d
Neftali Hernandez <neftaligow01@gmail.com>
2025-12-01 11:41:20 -0600

    ✏️ (fix): Env variables
    Change the variables

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL ?? "http://localhost:8080",
+    NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL ?? "http://localhost:3000",
+  },
+};
 
 export default nextConfig;
